c48c678afe8d9ed033d1617a5a880912d6a87c59
4n12i <an12inakamu12a@gmail.com>
2024-01-14 11:30:43 +0000

    step1: Update

--- src/test/step1.rs
+++ src/test/step1.rs
@@ -6,9 +6,12 @@ use crate::net::net_shutdown;
 use crate::test::data::TEST_DATA;
 use crate::util::tracing_init;
 use anyhow::Result;
+use signal_hook::consts::SIGINT;
+use signal_hook::iterator::Signals;
 use std::sync::atomic::AtomicBool;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
+use std::thread;
 use std::thread::sleep;
 use std::time::Duration;
 use tracing::warn;
@@ -16,12 +19,18 @@ use tracing::warn;
 pub fn run() -> Result<()> {
     tracing_init();
 
+    // Flag to detect Ctrl-C
     let running = Arc::new(AtomicBool::new(true));
+
+    // Handler setting
     let r = Arc::clone(&running);
-    ctrlc::set_handler(move || {
-        r.store(false, Ordering::SeqCst);
-    })
-    .expect("Error setting Ctrl-C handler");
+    let mut signals = Signals::new([SIGINT]).expect("Failed to create signal handler");
+    thread::spawn(move || {
+        for _ in signals.forever() {
+            r.store(false, Ordering::SeqCst);
+        }
+    });
+
     warn!("Press Ctrl-C to exit.");
 
     let mut net_devices = Vec::new();
@@ -29,9 +38,8 @@ pub fn run() -> Result<()> {
     dummy_init(&mut net_devices)?;
     net_run(&mut net_devices)?;
 
-    let dev = net_devices.pop().unwrap();
     while running.load(Ordering::SeqCst) {
-        net_device_output(&dev, 0x0800, TEST_DATA, None)?;
+        net_device_output(&net_devices[0], 0x0800, TEST_DATA, None)?;
         sleep(Duration::from_secs(1));
     }
     net_shutdown(&mut net_devices)?;
